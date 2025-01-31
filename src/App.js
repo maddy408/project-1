024b6584786dedda6d4ade18bb490b50f688716d
maddy408 <madhavi1918bca@gmail.com>
2025-01-31 17:09:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+
+function App() {
+  return (
+    <div className='app'>
+    <Header />
+    <Home />
+    <About />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
