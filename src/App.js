039000a0b2738dd178f39fb973717e1e4ba8b3b2
maddy408 <madhavi1918bca@gmail.com>
2025-02-01 10:38:02 +0530

    hover card

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
     <Header />
     <Home />
     <About />
+    <Skills />
+    <Projects/>
+    <Contact/>
     </div>
   );
 }
